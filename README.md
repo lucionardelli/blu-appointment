4d95a97d7be377aa6598a76b7bac0bbe5503acb0
Lucio Nardelli <lucionardelli@gmail.com>
2025-08-05 19:19:52 -0300

    move secrets to .env

---
+++ README.md
@@ -0,0 +1,5 @@
+## .env file
+
+  SECRET_KEY=your_super_secret_key_for_jwt
+  #You can generate one with: `from cryptography.fernet import Fernet; Fernet.generate_key().decode()`
+  FERNET_KEY=your_fernet_key
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,10 +5,10 @@ class Settings(BaseSettings):
     # This key is used for encrypting/decrypting sensitive data like medical history.
     # It MUST be a 32-byte URL-safe base64-encoded string.
     # You can generate one with: `from cryptography.fernet import Fernet; Fernet.generate_key().decode()`
-    FERNET_KEY: str = "N3mB8BZBCH0ROIT7PUy4d7dG50ED5N1Wse0t5WgB88g="
+    FERNET_KEY: str = "super_secret_key_for_encryption"
 
     # This key is for signing JWTs
-    SECRET_KEY: str = "your_super_secret_key_for_jwt"  # noqa: S105
+    SECRET_KEY: str = "super_secret_key_for_jwt"  # noqa: S105
 
     DATABASE_NAME: str = "blu.db"
     DATABASE_URL: str = f"sqlite:///db/{DATABASE_NAME}"
