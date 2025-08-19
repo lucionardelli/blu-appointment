7fcb351c6d621273d87f478f2d5cbc620479378b
Lucio Nardelli <lucionardelli@gmail.com>
2025-08-18 23:05:00 -0300

    Logout on 401s

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
+import router from "@/router";
 
 const instance = axios.create({
   baseURL: "http://localhost:8000/api/v1",
@@ -14,6 +15,18 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      const authStore = useAuthStore();
+      authStore.logout();
+      router.push("/login");
+    }
     return Promise.reject(error);
   },
 );
